6f43b693c254c4c47b71db8df1eadae60a48870b
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-14 19:01:38 +0200

    added: folder order

---
+++ src/builder.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+function build() {
+	var folders = [
+		'./0__utils/',
+		'./1__validations/',
+
+		'./2__models/',
+		'./3__views/',
+
+		'./4__connections/',
+		'./5__services/',
+		'./6__controllers/',
+
+		'./main.js'
+	];
+}
