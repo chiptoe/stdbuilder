05925d2743bff23ebf39657ac8a8c7e7c62c3140
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-14 20:04:48 +0200

    added: test

---
+++ tests/builder.test.js
@@ -0,0 +1,25 @@
+var { test, expect } = global;
+var { build } = require('../src/builder');
+
+test('Concat all project files in order', () => {
+	var result = build('./tests/builder/project/');
+	var expected = `(function() {
+	// 0__utils
+
+	// 1__validations
+
+	// 2__models
+
+	// 3__views
+
+	// 4__connections
+
+	// 5__services
+
+	// 6__controllers
+
+	// 7__main.js
+}());`;
+	console.log('result:' + result);
+	expect(result).toBe(expected);
+});
