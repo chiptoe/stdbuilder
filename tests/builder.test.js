0d3e9fcc835f2638a464200b88dedf99c3c974ef
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-14 22:33:54 +0200

    fixed: layers numbers in tests

--- tests/builder.test.js
+++ tests/builder.test.js
@@ -7,26 +7,26 @@ fs.copyFileSync('./src/noconflict.js', './tests/builder/project/0__noconflict.js
 test('Concat all project files in order', () => {
 	var result = build('./tests/builder/project/');
 	var expected = `(function() {
-	// 0__utils
+	// 1__utils
 	set__('Utils', (function() {
 		return {
 			myFunc: () => {}
 		};
 	}()));
 
-	// 1__validations
+	// 2__validations
 
-	// 2__models
+	// 3__models
 
-	// 3__views
+	// 4__views
 
-	// 4__connections
+	// 5__connections
 
-	// 5__services
+	// 6__services
 
-	// 6__controllers
+	// 7__controllers
 
-	// 7__main.js
+	// 8__main.js
 }());`;
 	expect(result).toBe(expected);
 });
