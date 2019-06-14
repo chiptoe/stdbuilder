f4069304f0e8d2671c379cede91f1df9bf09e49d
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-14 19:24:29 +0200

    +builder

--- src/builder.js
+++ src/builder.js
@@ -1,4 +1,6 @@
 // @ts-check
+var fs = require('fs');
+var Path = require('path');
 
 function build() {
 	var folders = [
@@ -12,6 +14,27 @@ function build() {
 		'./5__services/',
 		'./6__controllers/',
 
-		'./main.js'
+		'./7__main.js'
 	];
+
+	var builder = '';
+
+	folders.forEach((path) => {
+		if (path.lastIndexOf('.js') === -1) {
+			var filenames = fs.readdirSync(path);
+			filenames.forEach((filename) => {
+				builder += indentFileContent(Path.join(path, filename));
+			});
+		}
+		else {
+			builder += indentFileContent(path);
+		}
+	});
+
+	return builder;
+}
+
+function indentFileContent(filepath) {
+	var lines = fs.readFileSync(filepath, 'utf8').split('\n');
+	return lines.map((line) => '\t' + line).join('\n');
 }
