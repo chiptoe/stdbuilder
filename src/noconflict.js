76687b98cac9b12b8e58e17fc428eaf5400eccce
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-15 09:04:20 +0200

    updated: noconflict

--- src/noconflict.js
+++ src/noconflict.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-var __noconflict__ = 'PUT_MD5_HASH_HERE';
+var noconflict__ = 'PUT_MD5_HASH_HERE';
 
-var __global__ = (typeof(global) === 'object' ? global : window);
-__global__[__noconflict__] = {};
+var global__ = (typeof(global) === 'object' ? global : window);
+global__[noconflict__] = {};
 
 // @ts-ignore
-__global__.set__ = function(k, v) { __global__[__noconflict__][k] = v; };
+global__.set__ = function(k, v) { global__[noconflict__][k] = v; };
 // @ts-ignore
-__global__.get__ = function(k) { return __global__[__noconflict__][k]; };
+global__.get__ = function(k) { return global__[noconflict__][k]; };
