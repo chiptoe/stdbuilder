b69a6f27bd9ca019ae1b8710ca15f119d459558f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-15 11:19:53 +0200

    +test: Save bundle to file if --dist flag is provided

--- tests/builder.test.js
+++ tests/builder.test.js
@@ -1,8 +1,11 @@
 var { describe, test, expect } = global;
 var { build, prepareBasepath, tryReportTerserError } = require('../src/builder');
 var fs = require('fs');
+const { execSync } = require('child_process');
 
 fs.copyFileSync('./src/noconflict.js', './tests/builder/project/0__noconflict.js');
+
+execSync('rm -rf ./tests/builder/project/dist/');
 
 test('Concat all project files in order', () => {
 	var result = build('./tests/builder/project/');
@@ -51,6 +54,16 @@ test('Concat all project files in order and minify', () => {
 
 	var result = build('./tests/builder/project/');
 	var expected = '(function(){var noconflict__="PUT_MD5_HASH_HERE";var global__=typeof global==="object"?global:window;global__[noconflict__]={};global__.set__=function(k,v){global__[noconflict__][k]=v};global__.get__=function(k){return global__[noconflict__][k]};set__("Utils",function(){return{myFunc:()=>{}}}());return{myFunc:get__("Utils").myFunc}})();';
+	expect(result).toBe(expected);
+});
+
+test('Save bundle to file if --dist flag is provided', () => {
+	process.argv.push('--min');
+	process.argv.push('--dist');
+
+	build('./tests/builder/project/');
+	var expected = '(function(){var noconflict__="PUT_MD5_HASH_HERE";var global__=typeof global==="object"?global:window;global__[noconflict__]={};global__.set__=function(k,v){global__[noconflict__][k]=v};global__.get__=function(k){return global__[noconflict__][k]};set__("Utils",function(){return{myFunc:()=>{}}}());return{myFunc:get__("Utils").myFunc}})();';
+	var result = fs.readFileSync('./tests/builder/project/dist/bundle.js', 'utf8');
 	expect(result).toBe(expected);
 });
 
