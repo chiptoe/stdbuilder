0cf13b1fc3252bc821f2cf4ba77acd0fafb46386
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-15 09:27:48 +0200

    added: build config

---
+++ .build.js
@@ -0,0 +1,17 @@
+module.exports = {
+	build: [
+		'./0__noconflict.js',
+
+		'./1__utils/',
+		'./2__validations/',
+
+		'./3__models/',
+		'./4__views/',
+
+		'./5__connections/',
+		'./6__services/',
+		'./7__controllers/',
+
+		'./8__main.js'
+	]
+};
