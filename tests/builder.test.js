ab4dfd07f96919fe41b03b1f7ab2c66e732d186b
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-06-15 07:52:17 +0200

    fixed test: concat all project files in order

--- tests/builder.test.js
+++ tests/builder.test.js
@@ -7,6 +7,17 @@ fs.copyFileSync('./src/noconflict.js', './tests/builder/project/0__noconflict.js
 test('Concat all project files in order', () => {
 	var result = build('./tests/builder/project/');
 	var expected = `(function() {
+	/* eslint-disable no-underscore-dangle */
+	var __noconflict__ = 'PUT_MD5_HASH_HERE';
+
+	var __global__ = (typeof(global) === 'object' ? global : window);
+	__global__[__noconflict__] = {};
+
+	// @ts-ignore
+	__global__.set__ = function(k, v) { __global__[__noconflict__][k] = v; };
+	// @ts-ignore
+	__global__.get__ = function(k) { return __global__[__noconflict__][k]; };
+
 	// 1__utils
 	set__('Utils', (function() {
 		return {
